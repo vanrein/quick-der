362c7ec8186f4f99327970e861d59f97fd598eb2
Adriaan de Groot <groot@kde.org>
2017-01-25 17:46:29 +0100

    CMake: create .c files for include-testing and add them to tests.

--- cmake/MacroASN1Header.cmake
+++ cmake/MacroASN1Header.cmake
@@ -10,13 +10,22 @@
 #    add_custom_target(my-headers ALL)
 #    add_asn1_headers(my-headers rfc1 rfc2)
 #
-# This snipper requires files rfc1.asn1 and rfc2.asn1 to exist.
+# This snippet requires files rfc1.asn1 and rfc2.asn1 to exist.
+#
+# Using these macros requires MacroAddTestBuilder as well.
 
 macro(add_asn1_header _headername)
 	add_custom_target (${_headername}.h
 		${CMAKE_SOURCE_DIR}/tool/asn2quickder.py ${CMAKE_CURRENT_SOURCE_DIR}/${_headername}.asn1
 		COMMENT "Build include file ${_headername}.h from ASN.1 spec"
 		SOURCES ${_headername}.asn1)
+	set(ASN1_HEADER_NAME ${_headername})
+	configure_file(header-test.c.in
+		"${CMAKE_CURRENT_BINARY_DIR}/${_headername}.c" @ONLY)
+	add_executable(${_headername}-test EXCLUDE_FROM_ALL ${CMAKE_CURRENT_BINARY_DIR}/${_headername}.c)
+	target_include_directories(${_headername}-test PUBLIC ${CMAKE_SOURCE_DIR}/include)
+	add_dependencies(build-tests ${_headername}-test)
+	add_test(${_headername}-test ${_headername}-test)
 	install(FILES ${_headername}.h DESTINATION include/quick-der)
 endmacro()
 
