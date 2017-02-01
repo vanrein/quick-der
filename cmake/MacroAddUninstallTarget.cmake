84ab3b8494cf4fc1aa3ad069daf7a2a199197bdb
Adriaan de Groot <groot@kde.org>
2017-02-01 09:52:01 +0100

    CMake: support uninstall target

---
+++ cmake/MacroAddUninstallTarget.cmake
@@ -0,0 +1,15 @@
+# Add an uninstall target, as described on the CMake wiki.
+# Include this file, then call add_uninstall_target().
+# Requires a top-level cmake/ directory containing this
+# macro file and a cmake_uninstall.cmake.in.
+
+macro(add_uninstall_target)
+	# uninstall target
+	configure_file(
+		"${CMAKE_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+		"${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
+		IMMEDIATE @ONLY)
+
+	add_custom_target(uninstall
+		COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/cmake_uninstall.cmake)
+endmacro()
