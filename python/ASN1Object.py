6226a5be261150734584d5d411c899a2579cbb19
Rick van Rein <rick@openfortress.nl>
2017-03-10 14:42:12 +0000

    The ASN1Object now shares data between nested instances, for later der_pack()

--- python/ASN1Object.py
+++ python/ASN1Object.py
@@ -1,9 +1,22 @@
+#DONE# share the bindata and ofslen structures with sub-objects
+#TODO# add the packer data to the ASN1Object
+#TODO# add a der_pack() method
+#TODO# generate rfc1234.TypeName classes (or modules, or der_unpack functions)
+#TODO# deliver ASN1Object from the der_unpack() called on rfc1234.TypeName
+
+
+# The ASN1Object is a nested structure of class, accommodating nested fields.
+# Nesting instances share the bindata and ofslen structures, which they modify
+# to retain sharing.  The reason for this is that a future der_pack() on the
+# class must use changes made in the nested objects as well as the main one.
+
 class ASN1Object (object):
 
-	def __init__ (self, bindata='', ofslen=[], structure={}):
-		ASN1Object.bindata = bindata
-		ASN1Object.ofslen = ofslen
+	def __init__ (self, der_packer='\x00', structure={}, ofslen=[], bindata=['']):
+		ASN1Object.der_packer = der_packer
 		ASN1Object.structure = structure
+		ASN1Object.ofslen = ofslen
+		ASN1Object.bindata = bindata
 		for (k,v) in structure.items ():
 			if type (k) != type (""):
 				raise Exception ("ASN.1 structure keys can only be strings")
@@ -21,32 +34,36 @@ class ASN1Object (object):
 		if not ASN1Object.structure.has_key (name):
 			raise AttributeError (name)
 		val = str (val)
-		ASN1Object.ofslen [ASN1Object.structure [name]] = ( len(ASN1Object.bindata), len (val) )
-		ASN1Object.bindata = ASN1Object.bindata + val
+		siz = len (val)
+		ofs = sum (map (len, ASN1Object.bindata))
+		ASN1Object.ofslen [ASN1Object.structure [name]] = (ofs,siz)
+		ASN1Object.bindata.append (val)
 
 	def __getattr__ (self, name):
 		if not ASN1Object.structure.has_key (name):
 			raise AttributeError (name)
 		idx = ASN1Object.structure [name]
-		if idx < len (ASN1Object.ofslen):
-			(ofs,siz) = ASN1Object.ofslen [ASN1Object.structure [name]]
-		else:
-			(ofs,siz) = (0,0)
-		return ASN1Object.bindata [ofs:ofs+siz]
+		(ofs,siz) = ASN1Object.ofslen [ASN1Object.structure [name]]
+		elm = 0
+		while ofs >= len (ASN1Object.bindata [elm]):
+			ofs = ofs - len (ASN1Object.bindata [elm])
+			elm = elm + 1
+		return ASN1Object.bindata [elm] [ofs:ofs+siz]
 
 
 # class LDAPMessage (ASN1Object):
 if True:
 
-	bindata = 'Hello World'
-	ofslen = [ (0,5), (6,5) ]
+	der_packer = '\x00'
 	structure = { 'hello': 0, 'world': 1 }
+	ofslen = [ (0,5), (6,5) ]
+	bindata = ['Hello World']
 
 	# def unpack (self):
 	# 	return ASN1Object (bindata=self.bindata, ofslen=self.ofslen, structure=self.structure)
 
 	def unpack ():
-		return ASN1Object (bindata=bindata, ofslen=ofslen, structure=structure)
+		return ASN1Object (der_packer=der_packer, bindata=bindata, ofslen=ofslen, structure=structure)
 
 
 # 
