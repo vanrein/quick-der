5ed72e0603930b2735954c2b9bad97fcdacd10a2
Adriaan de Groot <groot@kde.org>
2017-09-01 13:26:07 +0200

    Python: simplify build
    The intermediate targets can be simplified, and we can drop
    some of them when building Quick-DER.

--- cmake/MacroASN1Module.cmake
+++ cmake/MacroASN1Module.cmake
@@ -47,19 +47,18 @@ macro(add_asn1_module _modulename _groupname)
 # and python/quick_der/modulename.py
 # and install the header file to include/quick-der/modulename.h.
 	set(_ppath $ENV{PYTHONPATH})
-	add_custom_command (OUTPUT quick-der/${_modulename}.h
+	add_custom_command (OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/quick-der/${_modulename}.h
 		COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${CMAKE_SOURCE_DIR}/python:${_ppath} ${CMAKE_SOURCE_DIR}/tool/asn2quickder.py -l c ${asn1module_asn2quickder_options} ${CMAKE_CURRENT_SOURCE_DIR}/${_modulename}.asn1
 		DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_modulename}.asn1
 		WORKING_DIRECTORY quick-der
 		COMMENT "Build include file ${_modulename}.h from ASN.1 spec")
-	add_custom_target(${_modulename}_asn1_h DEPENDS quick-der/${_modulename}.h)
 	add_custom_command (OUTPUT ${CMAKE_BINARY_DIR}/python/quick_der/${_modulename}.py
 		COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${CMAKE_SOURCE_DIR}/python:${_ppath} ${CMAKE_SOURCE_DIR}/tool/asn2quickder.py -l python ${asn1module_asn2quickder_options} ${CMAKE_CURRENT_SOURCE_DIR}/${_modulename}.asn1
 		DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_modulename}.asn1
 		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/python/quick_der
 		COMMENT "Build Python script ${_modulename}.py from ASN.1 spec")
-	add_custom_target(${_modulename}_asn1_py DEPENDS ${CMAKE_BINARY_DIR}/python/quick_der/${_modulename}.py)
 	install(FILES ${CMAKE_CURRENT_BINARY_DIR}/quick-der/${_modulename}.h DESTINATION include/quick-der)
+
 # Also add a test that builds against that module
 	set(ASN1_MODULE_NAME ${_modulename})
 	set(ASN1_HEADER_NAME quick-der/${_modulename})
@@ -67,7 +66,14 @@ macro(add_asn1_module _modulename _groupname)
 	configure_file(module-test.py.in ${CMAKE_CURRENT_BINARY_DIR}/${_modulename}-test.py @ONLY)
 	add_executable(${_modulename}-test-h ${CMAKE_CURRENT_BINARY_DIR}/${_modulename}.c)
 	target_include_directories(${_modulename}-test-h PUBLIC ${CMAKE_SOURCE_DIR}/include ${CMAKE_CURRENT_BINARY_DIR})
-	add_dependencies(${_modulename}-test-h ${_groupname})
+
+	add_custom_target(${_modulename}_asn1_h DEPENDS 
+		${CMAKE_CURRENT_BINARY_DIR}/quick-der/${_modulename}.h
+		${CMAKE_BINARY_DIR}/python/quick_der/${_modulename}.py
+	)
+	add_dependencies(${_modulename}-test-h ${_modulename}_asn1_h)
+	add_dependencies(${_groupname} ${_module}_asn1_h)
+
 	add_test(${_modulename}-test-h ${_modulename}-test-h)
 	add_test(${_modulename}-test-py python ${_modulename}-test.py)
 endmacro()
@@ -77,7 +83,6 @@ macro(add_asn1_modules _groupname)
 	file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/quick-der)
 	foreach (_module ${ARGN})
 		add_asn1_module(${_module} ${_groupname})
-		add_dependencies(${_groupname} ${_module}_asn1_h ${_module}_asn1_py)
 	endforeach()
 endmacro()
 
