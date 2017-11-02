c1e110c0649fb72c4539e927b3471658fd3afa2f
Adriaan de Groot <groot@kde.org>
2017-11-02 23:22:16 +0100

    Wrangle Travis
     - add in a script for building Quick DER including dependencies
     - let Travis run the script, instead of putting the whole build
       instruction in the .travis.yml file.

---
+++ ci/travis-continuous.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+#
+# Travis CI script for use on every-commit:
+#  - build and install Arpa2CM (dependency)
+#  - build and install Quick DER
+#
+test -n "$BUILDDIR" || exit 1
+test -n "$SRCDIR" || exit 1
+
+test -d "$BUILDDIR" || exit 1
+test -d "$SRCDIR" || exit 1
+test -f "$SRCDIR/CMakeLists.txt" || exit 1
+
+cd "$BUILDDIR" || exit 1
+
+### First, ARPA2CM
+#
+#
+git clone https://github.com/arpa2/arpa2cm/ arpa2cm_build
+( cd arpa2cm_build && mkdir build && cd build && cmake .. && make && make install )
+
+### Second, Quick DER
+#
+#
+cmake .. && make -j
+
