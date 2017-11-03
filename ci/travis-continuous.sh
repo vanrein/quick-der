cbcf88b0b3b1a58f10c48cc6ff1cb5c6376d6fe1
Gijs Molenaar <gijs@pythonic.nl>
2017-11-03 09:45:07 +0200

    sudo the install

--- ci/travis-continuous.sh
+++ ci/travis-continuous.sh
@@ -17,7 +17,7 @@ cd "$BUILDDIR" || exit 1
 #
 #
 git clone https://github.com/arpa2/arpa2cm/ arpa2cm_build || exit 1
-( cd arpa2cm_build && mkdir build && cd build && cmake .. && make && make install ) || exit 1
+( cd arpa2cm_build && mkdir build && cd build && cmake .. && make && sudo make install ) || exit 1
 
 ### Second, Quick DER
 #
