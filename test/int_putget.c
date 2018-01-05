533057e69515c0746ed566615a3545ef8d2dbe04
Adriaan de Groot <groot@kde.org>
2018-01-05 16:14:41 +0100

    Tests: fix printf format strings.
     - %l isn't a thing on its own
     - size_t should be %zu

--- test/int_putget.c
+++ test/int_putget.c
@@ -27,9 +27,9 @@ int unsigned_tests (void) {
 		int match = (val == tests [i]);
 		ok = ok && fit && match;
 		if (fit != 0) {
-			fprintf (stderr, "Unsigned integer %ul took %d bytes %02x %02x... and does not fit in 32 bits anymore\n", tests [i], crs.derlen, crs.derptr [0], crs.derptr [1]);
+			fprintf (stderr, "Unsigned integer %u took %zu bytes %02x %02x... and does not fit in 32 bits anymore\n", tests [i], crs.derlen, crs.derptr [0], crs.derptr [1]);
 		} else if (match != 0) {
-			fprintf (stderr, "Unsigned integer %ul took %d bytes and came back as %ul\n", tests [i], crs.derlen, val);
+			fprintf (stderr, "Unsigned integer %u took %zu bytes and came back as %u\n", tests [i], crs.derlen, val);
 		}
 	}
 	return ok;
@@ -54,9 +54,9 @@ int signed_tests (void) {
 		int match = (val == tests [i]);
 		ok = ok && fit && match;
 		if (fit != 0) {
-			fprintf (stderr, "Signed integer %l took %d bytes %02x %02x... and does not fit in 32 bits anymore\n", tests [i], crs.derlen, crs.derptr [0], crs.derptr [1]);
+			fprintf (stderr, "Signed integer %d took %zu bytes %02x %02x... and does not fit in 32 bits anymore\n", tests [i], crs.derlen, crs.derptr [0], crs.derptr [1]);
 		} else if (match != 0) {
-			fprintf (stderr, "Signed integer %l took %d bytes and came back as %l\n", tests [i], crs.derlen, val);
+			fprintf (stderr, "Signed integer %d took %zu bytes and came back as %d\n", tests [i], crs.derlen, val);
 		}
 	}
 	return ok;
