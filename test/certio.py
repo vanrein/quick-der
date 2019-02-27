ce2f3b509d609d565f562fc481e0002419e15502
Adriaan de Groot <groot@kde.org>
2019-02-27 08:23:20 -0500

    Python wrangling (#70)
    * Docs: remind people to install asn1ate
    
    * Python: fix tests
    
     - Concatenate strings, avoid error message
           39: Traceback (most recent call last):
           39:   File "/mnt/src/arpa2/quick-der/test/certio.py", line 37, in <module>
           39:     print('type is ' + type (exts))
           39: TypeError: cannot concatenate 'str' and 'type' objects
    
    * Tests: get existing PYTHONPATH and use it
    
    * Simplify .gitignore -- these indicate source-dir pollution during build
    
    * CMake: fix library install on Windows
    
    Even shared-libs have a static-lib component on Windows. Reported
    by Henri Manson.
    
    * Python: use new PythonSupport module
    
     - bump ARPA2CM requirements
     - require CMake 3.2, same as ARPA2CM
     - massage Python build; reported by Henri Manson
    
    * Docs: point to the Python installation docs
    
    * Python: provide top-level convenience target
    
     - The top-level convenience target "python" does the python build,
       after installing the library. This is probably fragile.
    
    * Python: needs relative imports in Python3
    
    * Python: expand tests
    
     - Tests were failing because of ASCII output, where 0xa1-0xff (i.e. 161-255)
       was being left alone by repr() and then failing on output. Catch those
       cases and use a home-grown repr. Lines output that way are flagged with
       (*) so they are visible.
     - This is probably a consequence of treating str-objects as if they are
       bytes, and so a Python 3 issue more than anything else.
    
    * Python: expand tests of OIDs
    
     - Unpacking is failing the test:
       ```
           T=OID
           V='1.2'
           f(V)='*'
           pf(V)='1.05.2'
       ```
    
    * Python: force integer division, for Python3 compatibility
    
    * CMake: import PythonSupport from ARPA2CM
    
     - It may be premature to have the module in ARPA2CM, it probably
       needs a few iterations to mature first.
     - Bumping the required release of ARPA2CM brings new porting issues
       we want to avoid.
    
    * CMake: force use of source-based python script for build
    
     - The script might be installed by a previous version. Don't want
       that version to be used for the new build.
     - Also fixes corner case where CMake found and cached the installed
       version, which would then block the build if the installed version
       was removed.
    
    * CMake: let the python/ part use the common python interpreter
    
    * CMake: allow any Python
    
     - Although Windows only has Python3, we should support Python2
       where possible / needed as well. So use the 3-or-otherwise-2
       general FindPython module instead of FindPython3.

--- test/certio.py
+++ test/certio.py
@@ -29,12 +29,12 @@ if der_out != der_in:
 	sys.exit (1)
 
 print('TBSCERTIFICATE:')
-print('type is' + type (crt.tbsCertificate))
+print('type is' + str (type (crt.tbsCertificate)))
 print()
 
 exts = crt.tbsCertificate.extensions
 print('EXTENSIONS:')
-print('type is ' + type (exts))
+print('type is ' + str( type (exts)))
 for exti in range (len (exts)):
 	print('[' + str (exti) + '] ' + str (exts [exti].extnID if exts [exti] else '(no OID)') + ' ' + ('CRITICAL ' if exts [exti].critical else '') + str (exts [exti].extnValue if exts [exti].extnValue else '(no value)'))
 
