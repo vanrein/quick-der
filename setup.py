5eb2d8fc4848d4a2a87a195389ecfb8c023d0794
Rick van Rein <rick@openfortress.nl>
2019-03-09 10:36:36 +0000

    Incorporate asn2quickder output files as quick_der.rfc5280 includables

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from setuptools import setup, Extension, find_packages
-from os import path
+from os import path, mkdir
+from subprocess import check_call
+from tempfile import mkdtemp
+from atexit import register as atexit_register
 
 here = (path.dirname(path.realpath(__file__)))
 
@@ -13,6 +16,28 @@ extension = Extension(name='_quickder',
                           path.join(here, 'lib', 'der_pack.c')],
                       include_dirs=[path.join(here, 'include')],
                       )
+
+# Before we enter setup() we need to build the Python libraries
+# from their ASN.1 sources using the asn2quickder script.
+
+a2qd = mkdtemp ()
+dest_parent = path.join(a2qd, 'python')
+dest_pkgdir = path.join(a2qd, 'python', 'quick_der')
+orig_pkgdir = path.join(here, 'python', 'quick_der')
+a2qd_pkgdir = path.join(a2qd, 'python', 'testing')
+
+atexit_register (check_call, ['cmake', 'remove_directory', a2qd])
+
+mkdir (dest_parent)
+mkdir (dest_pkgdir)
+
+check_call (['cmake', here], cwd=a2qd)
+check_call (['cmake', '--build', a2qd])
+#RECURSIVE# check_call (['ctest', '-VV'], cwd=a2qd)
+check_call (['cmake', '-E', 'copy_directory', a2qd_pkgdir, dest_pkgdir], cwd=a2qd)
+check_call (['cmake', '-E', 'copy_directory', orig_pkgdir, dest_pkgdir], cwd=a2qd)
+
+#DEBUG# check_call (['ls', dest_pkgdir])
 
 setup(
     scripts=['python/scripts/asn1literate', 'python/scripts/asn2quickder'],
@@ -26,10 +51,10 @@ setup(
     url="https://github.com/vanrein/quick-der",
     version='1.2.2',
     ext_modules=[extension],
-    packages=find_packages(where='python'),
+    packages=find_packages(where=dest_parent),
     package_dir={
-        'quick_der': path.join ('python', 'quick_der'),
-         'tests': path.join ('python', 'tests'),
+        'quick_der': dest_pkgdir,
+         #DO_NOT_INSTALL# 'tests': path.join ('python', 'tests'),
     },
     install_requires=[
         'six',
