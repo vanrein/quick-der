70d87b6d4a87a1c1cec87681ab3aec417037882f
Rick van Rein <rick@openfortress.nl>
2019-03-13 09:22:06 +0000

    Improvements to setup.py (commented on lack of setuptools documentation)

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 
 from setuptools import setup, Extension, find_packages
-from os import path
+from os import path, environ
 from re import compile as re_compile
 from subprocess import check_call
 from tempfile import mkdtemp
 
 here = (path.dirname(path.realpath(__file__)))
 
-extension = Extension(name='_quickder',
-                      sources=[
-                          path.join(here, 'python', 'src', '_quickder.c'),
-                          path.join(here, 'lib', 'der_header.c'),
-                          path.join(here, 'lib', 'der_unpack.c'),
-                          path.join(here, 'lib', 'der_pack.c')],
-                      include_dirs=[path.join(here, 'include')],
-                      )
-
 # Before we enter setup() we need to build the Python libraries
 # from their ASN.1 sources using the asn2quickder script.
 # We work around CMake, but then need regexp matching on scripts.
 # CMake however, is quite troublesome in multilinux targets.
 
+# In terms of Python setuptools, this is not perfect either.  We
+# should have made an Extension next to the one for the C library.
+# But then again, we should have found documentation on how this
+# is done.  If we missed it, please send us a reference!
+
+environ ['PYTHONPATH'] = '%s:%s' % (path.join (here, 'python'), environ.get ('PYTHONPATH', ''))
 add1_open_re = re_compile ('[ \t\n]add_asn1_modules[ \t\n]*\(')
 asn2cmd = [ path.join (here, 'python', 'scripts', 'asn2quickder'), '-l', 'python' ]
 for asn1dir in ['rfc', 'itu', 'arpa2']:
@@ -36,8 +33,20 @@ for asn1dir in ['rfc', 'itu', 'arpa2']:
 			continue
 		for asn1spec in mtch [:closed].split () [1:]:
 			asn1path = path.join (here, asn1dir, asn1spec + '.asn1')
-			print ('Generating module \"quick_der.%s\" from %s' % (asn1spec,asn1path))
-			check_call (asn2cmd + [ asn1path ], cwd='/tmp')
+			print ('Generating import module \"quick_der.%s\" from %s%s%s.asn1' % (asn1spec,asn1dir,path.sep,asn1spec))
+			check_call (asn2cmd + [ asn1path ], cwd=path.join (here, 'python', 'quick_der'))
+
+# We proceed with the more proper parts of the setup.
+# That is, the parts for which we could find documentation.
+
+extension = Extension(name='_quickder',
+                      sources=[
+                          path.join(here, 'python', 'src', '_quickder.c'),
+                          path.join(here, 'lib', 'der_header.c'),
+                          path.join(here, 'lib', 'der_unpack.c'),
+                          path.join(here, 'lib', 'der_pack.c')],
+                      include_dirs=[path.join(here, 'include')],
+                      )
 
 setup(
     scripts=['python/scripts/asn1literate', 'python/scripts/asn2quickder'],
@@ -53,7 +62,7 @@ setup(
     ext_modules=[extension],
     packages=find_packages(where='python'),
     package_dir={
-        'quick_der': 'quick_der',
+        'quick_der': path.join (here, 'python', 'quick_der'),
     },
     install_requires=[
         'six',
